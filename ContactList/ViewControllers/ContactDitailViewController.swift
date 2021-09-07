85f91324d27ff0d0ae51b8cc4a4a52abdccb6aaf
Dmitri Gordeev <dimapushkin@gmail.com>
2021-09-07 23:17:48 +0300

    setup contact detail VC

--- ContactList/ViewControllers/ContactDitailViewController.swift
+++ ContactList/ViewControllers/ContactDitailViewController.swift
@@ -9,8 +9,17 @@ import UIKit
 
 class ContactDitailViewController: UIViewController {
 
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var phoneNumLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fullNameLabel.layer.cornerRadius = 15
+        fullNameLabel.clipsToBounds = true
+     
 
     
     }
