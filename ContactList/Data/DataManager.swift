aace49dcda37a12003fb332ba9da9aeb1fe59fc3
Dmitri Gordeev <dimapushkin@gmail.com>
2021-09-07 23:55:27 +0300

    create arrays of names, surnames, phones and emails

--- ContactList/Data/DataManager.swift
+++ ContactList/Data/DataManager.swift
@@ -6,4 +6,51 @@
 //
 
 import Foundation
- 
+
+class DataManager {
+
+let names = [
+    "Ivan",
+    "Alex",
+    "Antony",
+    "Pier",
+    "Gustav",
+    "Basil",
+    "Joe",
+    "Donald"
+]
+
+let surnames = [
+    "Petroff",
+    "Van Basten",
+    "Ivanov",
+    "Picard",
+    "Berg",
+    "Olaffson",
+    "Lampard",
+    "Chan"
+]
+
+let cellPhoneNums = [
+    "+7(921)123-45-67",
+    "+7(925)146-55-76",
+    "+7(926)323-75-17",
+    "+7(989)539-25-37",
+    "+7(963)773-19-24",
+    "+7(961)909-95-47",
+    "+7(903)221-84-07",
+    "+7(901)321-74-01",
+]
+
+let eMails = [
+    "1box@mail.ru",
+    "2box@gmail.com",
+    "3box@yandex.ru",
+    "4box@yahoo.ru",
+    "5box@rambler.ru",
+    "6box@hotmail.com",
+    "7box@apple.com",
+    "007@почта.рф",
+]
+    
+}
